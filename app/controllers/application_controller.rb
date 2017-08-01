8651bbcf4be043adf6cfc773812c9afcdb969879
Adam Coffman <acc@fastmail.com>
2017-08-01 13:02:59 -0500

    initial group messaging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
   end
 
   def lunch
+    channel_id = params[:channel_id]
+    client = Slack::Web::Client.new
+    resp = client.chat_postMessage(channel: channel_id,
+                            text: 'Who is in for lunch? (react with :+1:)',
+                            as_user: true)
+
+    CreateGroup.perform_later(channel_id,
+                              resp.message.ts,
+                              wait_until: 1.minute.from.now)
+
+
+
     render plain: 'lunch? that sounds good!'
   end
 end
---
+++ app/jobs/create_group.rb
@@ -0,0 +1,44 @@
+class CreateGroup < ApplicationJob
+  def perform(channel_id, message_id)
+    users_to_notify = get_users_who_reacted(channel_id, message_id)
+    group_id = create_group(users_to_notify)
+    notify_users(group_id)
+  end
+
+  private
+  def client
+    @client ||= Slack::Web::Client.new
+  end
+
+  def get_users_who_reacted(channel_id, message_id)
+    reactions_response = client.reactions_get(
+      channel: channel_id,
+      timestamp: message_id
+    )
+
+    if reactions_response.ok
+      reactions_response.message
+        .reactions
+        .select { |r| r.name == '+1' }
+        .flat_map { |r| r.users }
+        .uniq
+    else
+      []
+    end
+  end
+
+  def create_group(users)
+    resp = client.mpim_open(users: users)
+    if resp.ok
+      resp.group.id
+    else
+      nil
+    end
+  end
+
+  def notify_users(group_id)
+    client.chat_postMessage(channel: group_id,
+                            text: "Hey, you're stuck going to lunch together",
+                            as_user: true)
+  end
+end
