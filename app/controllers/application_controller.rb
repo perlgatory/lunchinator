885ddf8579b36bd98a0dd3f1b2635dfd63f9dfbd
Adam Coffman <acc@fastmail.com>
2017-08-08 12:35:06 -0500

    lunchinator is channel aware. sort of.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,24 @@ class ApplicationController < ActionController::Base
   def lunch
     channel_id = params[:channel_id]
     client = Slack::Web::Client.new
-    resp = client.chat_postMessage(channel: channel_id,
-                            text: 'Who is in for lunch? (react with :+1:)',
-                            as_user: true)
+    if already_joined_channel?(client, channel_id)
+      resp = client.chat_postMessage(channel: channel_id,
+                              text: 'Who is in for lunch? (react with :+1:)',
+                              as_user: true)
 
-    CreateGroup
-      .set(wait_until: 1.minute.from_now)
-      .perform_later(channel_id, resp.message.ts)
+      CreateGroup
+        .set(wait_until: 1.minute.from_now)
+        .perform_later(channel_id, resp.message.ts)
 
-    render plain: 'lunch? that sounds good!'
+      render plain: 'lunch? that sounds good!'
+    else
+      render plain: "Looks like I'm not invited :cry:"
+    end
+  end
+
+  private
+  def already_joined_channel?(client, channel_id)
+    channel_info = client.channels_info(channel: channel_id)
+    channel_info.channel.is_member
   end
 end
