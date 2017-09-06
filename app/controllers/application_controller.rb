901ab91da8337722e54e36b20ec86cf73835418f
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-09-06 12:52:06 -0500

    add in snark

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,26 @@ class ApplicationController < ActionController::Base
   def lunch
     channel_id = params[:channel_id]
     initiating_user_id = params[:user_id]
+    app_text = params[:text]
+    lunch_time = Chronic.parse(app_text.strip.gsub(/^\s*(at|@)\s+/i, ''))
+    if lunch_time < Time.now
+      render plain: "#{lunch_time} is in the past. Please pick a different time for lunch... or build a time machine."
+    elsif lunch_time < Time.now + 10.minutes
+      assemble_time = lunch_time
+    else
+      assemble_time = lunch_time - 10.minutes
+    end
     client = Slack::Web::Client.new
     channel = Channel.new(channel_id)
     status = channel.client_status(client)
     if status == :already_joined
       resp = client.chat_postMessage(channel: channel_id,
-                              text: 'Who is in for lunch? (react with :+1:)',
+                              text: "#{app_text} (#{lunch_time.to_s}), who is in for lunch? (react with :+1:)",
                               as_user: true)
       response_ts = resp.message.ts
       client.reactions_add(name: '+1', channel: channel_id, timestamp: response_ts)
       CreateGroup
-        .set(wait_until: 10.minutes.from_now)
+        .set(wait_until: assemble_time)
         .perform_later(channel_id, initiating_user_id, response_ts)
       render plain: 'lunch? that sounds good!'
     elsif status == :not_joined
