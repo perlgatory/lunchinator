9634a49bc4e5a1b1d094f02a1ad07c86a643635f
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-09-06 13:47:56 -0500

    wip: time zone support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,27 @@ class ApplicationController < ActionController::Base
   end
 
   def lunch
+    client = Slack::Web::Client.new
     channel_id = params[:channel_id]
     initiating_user_id = params[:user_id]
+    user_time_zone = get_user_timezone(initiating_user_id, client)
     app_text = params[:text]
-    lunch_time = Chronic.parse(app_text.strip.gsub(/^\s*(at|@)\s+/i, ''))
-    if lunch_time < Time.now
-      render plain: "#{lunch_time} is in the past. Please pick a different time for lunch... or build a time machine."
-    elsif lunch_time < Time.now + 10.minutes
+    parsed_time = Chronic.parse(app_text.strip.gsub(/^\s*(at|@)\s+/i, ''))
+    lunch_time = ActiveSupport::TimeZone.new('America/Los_Angeles').local_to_utc(parsed_time)
+    now = Time.now.utc
+    if lunch_time < now
+      render plain: "#{parsed_time} is in the past. Please pick a different time for lunch... or build a time machine."
+      return
+    elsif lunch_time < now + 10.minutes
       assemble_time = lunch_time
     else
       assemble_time = lunch_time - 10.minutes
     end
-    client = Slack::Web::Client.new
     channel = Channel.new(channel_id)
     status = channel.client_status(client)
     if status == :already_joined
       resp = client.chat_postMessage(channel: channel_id,
-                              text: "#{app_text} (#{lunch_time.to_s}), who is in for lunch? (react with :+1:)",
+                              text: "#{app_text} (#{parsed_time.to_s}), who is in for lunch? (react with :+1:)",
                               as_user: true)
       response_ts = resp.message.ts
       client.reactions_add(name: '+1', channel: channel_id, timestamp: response_ts)
@@ -33,5 +37,10 @@ class ApplicationController < ActionController::Base
     else
       render plain: "I'm not allowed in there :slightly_frowning_face:"
     end
+  end
+
+  def get_user_timezone(user_id, client)
+    # https://api.slack.com/methods/users.info <- get user timezone from here
+    nil
   end
 end
