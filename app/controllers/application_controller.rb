77ddaefaa24c10a6dbcbf9c75e8948c2fec08cce
Thomas Mooney <tmooney@genome.wustl.edu>
2018-03-13 12:44:01 -0500

    Use count-agnostic language in reply message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
       links = results.map do |item|
         client.chat_getPermalink(channel: item.channel_id, message_ts: item.message_id)['permalink']
       end
-      render plain: "Here are some existing lunch groups:\n" + links.join("\n") # TODO: pick up here
+      render plain: "You're not the first to plan lunch today!  Consider:\n" + links.join("\n") # TODO: pick up here
       # notify user of other valid group(s)
       return
     end
