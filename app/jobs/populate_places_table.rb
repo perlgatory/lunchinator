4b27043ac5598f1247192a971033f52a81cc2783
KK <kkrysiak@genome.wustl.edu>
2018-05-30 13:02:43 -0500

    Update google api call to return multiple pages

--- app/jobs/populate_places_table.rb
+++ app/jobs/populate_places_table.rb
@@ -1,7 +1,8 @@
 class PopulatePlacesTable < ApplicationJob
   def perform
     client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    client.spots(38.641230, -90.265204, :types => 'restaurant', :radius => 1500)
+    client.spots(38.641230, -90.265204, :types => 'restaurant', :radius => 1010, :multipage => 1)
     #TODO: fill database with results
+    # Multipage only allows 60 entries max
   end
 end
