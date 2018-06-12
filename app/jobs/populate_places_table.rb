5f5a4223f90c8d61d6fe95859d4e5eb254ee4200
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-06-12 13:19:17 -0500

    add spots to database

--- app/jobs/populate_places_table.rb
+++ app/jobs/populate_places_table.rb
@@ -1,8 +1,7 @@
 class PopulatePlacesTable < ApplicationJob
   def perform
     client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    client.spots(38.641230, -90.265204, :types => 'restaurant', :radius => 1010, :multipage => 1)
-    #TODO: fill database with results
-    # Multipage only allows 60 entries max
+    spots = client.spots(38.641230, -90.265204, :types => 'restaurant', :radius => 1010, :multipage => 1).map{ |spot| {name: spot.name, google_places_id: spot.id }}
+    spots.each { |spot| Place.where(spot).first_or_create }
   end
 end
