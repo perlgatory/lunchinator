b2c1b8779526d3d47b7e0d201193aa5bbcbfe10c
Thomas Mooney <tmooney@genome.wustl.edu>
2019-01-29 12:56:34 -0600

    Format the timestamp for updated lunchgroup messaging.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def format_time_for_user(time, user, client)
+    user_time_zone = user.timezone(client)
+    time.in_time_zone(user_time_zone).strftime('%H:%M:%S (%Z) %d-%b-%Y')
+  end
 end
--- app/jobs/depart_group.rb
+++ app/jobs/depart_group.rb
@@ -1,9 +1,10 @@
 class DepartGroup < ApplicationJob
+  include ApplicationHelper
+
   def perform(group_id)
     group = LunchGroup.find(group_id)
     group.update(status: 'departed')
-    user_time_zone = group.initiating_user.timezone(client)
-    lunch_time = group.departure_time.in_time_zone(user_time_zone)
+    lunch_time = format_time_for_user(group.departure_time, group.initiating_user, client)
     client.chat_update(
       channel: group.channel_id, ts: group.message_id,
       text: "A group has departed for #{group.destination_string} at #{lunch_time}. Contact #{group.initiating_user.username} to join."
